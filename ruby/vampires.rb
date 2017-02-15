594b30688a689d68ac1e8f474611b957dcd640c7
J Ted Stamps <tedst@comcast.net>
2017-02-15 10:00:31 -0800

    Add basic release 2 detection logic.  not fully debugged

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,20 +1,49 @@
 #Release 1: Interview with the Vampire
 
 puts "Please enter your name: "
-inputed_name=gets.chomp
+inputed_name = gets.chomp
 puts "How old are you?"
-age=gets.chomp.to_i
+age = gets.chomp.to_i
 puts "What year were you born?"
-year_born=gets.chomp.to_i
+year_born = gets.chomp.to_i
 
-#if current year minus birth year >=2 then something is fishy
+age_matches = false
+if (2017-year_born == age) ||  (2017-year_born == (age +1))
+  age_matches = true
+
 
 puts "Our company cafeteria serves garlic bread.  Should we order some for you? (yes/no)"
 if gets.chomp == "yes" 
-  garlic_bread_eater= true
+  garlic_bread_eater = true
 else
   garlic_bread_eater = false
 end
 
-puts "Would you like to enroll in teh company's health insurannce?  (yes/no)"
-insurance = gets.chomp
+puts "Would you like to enroll in the company's health insurance?  (yes/no)"
+if gets.chomp == "yes"
+  insurance = true
+else
+  insurance = false
+end
+
+vampire_guess = "Results Inconclusive" #set default to inconclusive, then update as info is processed.
+
+
+if age_matches == true
+  if garlic_bread_eater == true
+    if insurance == true
+      vampire_guess = "Probably NOT a vampire"
+    end
+  end
+elsif (garlic_bread_eater == false) ||  (insurance == false)     #age is wrong/mismatched going into this elsif
+    vampire_guess = "Probably a vampire"
+elsif (garlic_bread_eater == false) && (insurance == false)
+  vampire_guess = "Almost CERTAINLY a vampire!"
+end
+
+
+if inputed_name == "Drake Cula" || inputed_name == "Tu Fang"
+vampire_guess = "Definitely a VAMPIRE!" 
+end
+
+puts vampire_guess
