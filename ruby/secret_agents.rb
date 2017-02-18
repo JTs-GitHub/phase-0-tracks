8de2002d656fcf6b9e9d9d1409daa3203216c307
J Ted Stamps <tedst@comcast.net>
2017-02-18 10:47:14 -0800

    Add Release3 testing, and added conditional testing for edge case of z in encrypt method.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -23,19 +23,23 @@ DECRYPT pseudocode:
     -subtract the alphabet index by 1, and write that letter into the new string
     -repeat until all the letters have been converted into a new string
     -print the new string
-
-alphabet_index = alphabet.index(string_to_decrypt[offset])
-
 =end
+
 
 
 def encrypt(string_to_encrypt)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
-  index = 0
+  alphabet_index = 0   # integer var used to store alphabet's index for given letter.
+  offset = 0   # "offset" is replacing "index" var because there were too many "index" vars and methods. 
+  
   print "#{string_to_encrypt} becomes "
-  while index < string_to_encrypt.length
-    print alphabet[string_to_encrypt[index].next]
-    index += 1
+  while offset < string_to_encrypt.length
+    alphabet_index = alphabet.index(string_to_encrypt[offset])
+    if alphabet_index == 25          # adding condition check if string index == 25  (i.e. "z"), for edge case
+      alphabet_index = -1
+    end
+    print alphabet[alphabet_index + 1]
+    offset += 1
   end
   puts ""
 end
@@ -54,4 +58,10 @@ def decrypt(string_to_decrypt)
   puts ""
 end
 
+
+
+encrypt("abc")
+encrypt("zed")
+decrypt("bcd")
+decrypt("afe")
 
