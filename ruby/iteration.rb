6e5fa99637bb35fc0b15d1c2d59a876f4fa63798
J Ted Stamps <tedst@comcast.net>
2017-02-21 11:42:36 -0800

    ADD release 1 of pp 5.3

---
+++ ruby/iteration.rb
@@ -0,0 +1,17 @@
+
+
+#DEFINE METHODS
+def power                                            #method yields to block in DRIVER section
+  puts "we are now at the beginning of the method".  #debug code to track progress
+  yield(2 , 3)                                       #pass integers to the block code
+  puts "we are now after the block yeild statement". #debug code
+end
+
+#DRIVER CODE
+puts "we are about to call the method with our block"  #debug code
+power { |base, exponent| puts base**exponent }         # method call, passing in a block
+puts
+puts "and now we are after the block and method call"   #debug
+
+
+
