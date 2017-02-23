9379fbd24f589c5ca137dcc9723920fb11af49eb
J Ted Stamps <tedst@comcast.net>
2017-02-22 17:09:35 -0800

    Add release 2 method calls from documentation, conclude assignment 5,3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -24,7 +24,6 @@ puts "and now we are after the block and method call"   #debug
 
 =begin 
    # commenting out array methods to make way for hash methods.
-todo_list = [ "grocery shopping" , "dry cleaning" , "fill car with gas" , "take out trash", "pick up kids from school" ]  
 
 
     #DRIVER CODE 
@@ -57,6 +56,7 @@ todo_list = [ "grocery shopping" , "dry cleaning" , "fill car with gas" , "take 
     end
     p todo_list                              # verify changed array
 =end
+todo_list = [ "grocery shopping" , "dry cleaning" , "fill car with gas" , "take out trash", "pick up kids from school" ]  
 
 famous_opens = {
     moby_dick: "Call me Ishmael." ,
@@ -65,24 +65,59 @@ famous_opens = {
     the_invisible_man: "I am an invisible man."
 }
 
-p famous_opens                                                        # original hash
-puts "Now doing .each on the hash table's values to add elipsis"      
-famous_opens.each do |book, opening_verse|
-  puts "#{opening_verse}..."                                          # print out each value plus added ellipsis
-end
-p famous_opens                                                        # debug, expect unchanged hash
-puts                                                                  # debug spacing
+=begin 
 
-p famous_opens                                                         #original hash
-puts
-puts "Now doing .map on the hash table's values"
-h = famous_opens.map do |book, opening_verse|                          # .map method call on hash, expect new array returned to h with changed values.
-  puts "#{opening_verse} ..."
-  opening_verse = opening_verse + "     <guess the book title>"
-end
-p famous_opens                                                         #print hash, expect unchanged
-puts                                             
-puts h                                                                    # print new array with changed values 
+        # COMMENTING OUT Release 1 code to make room for Release 2 code
 
+        p famous_opens                                                        # original hash
+        puts "Now doing .each on the hash table's values to add elipsis"      
+        famous_opens.each do |book, opening_verse|
+          puts "#{opening_verse}..."                                          # print out each value plus added ellipsis
+        end
+        p famous_opens                                                        # debug, expect unchanged hash
+        puts                                                                  # debug spacing
+
+        p famous_opens                                                         #original hash
+        puts
+        puts "Now doing .map on the hash table's values"
+        h = famous_opens.map do |book, opening_verse|                          # .map method call on hash, expect new array returned to h with changed values.
+          puts "#{opening_verse} ..."
+          opening_verse = opening_verse + "     <guess the book title>"
+        end
+        p famous_opens                                                         #print hash, expect unchanged
+        puts                                             
+        puts h                                                                    # print new array with changed values 
+
+=end
+
+#iterating method that takes a block && deletes items based on a condition
+  #array
+    new_a = todo_list.reject { |task| task == "dry cleaning"}
+  #hash
+    famous_opens.reject! {|book, text| text == "Call me Ishmael."}
+
+
+#iterating method that takes a block && filters items that satisfy a condition
+  # array
+    new_a2 = todo_list.find_all { |task| task == "take out trash"}
+  # hash
+    famous_opens.keep_if { |book, text|  book == :moby_dick || book == :the_invisible_man}
+
+
+#a different iterating method that takes a block && filters/keeps items based on condition
+#array using a new array
+a = [1, 2, 3, 4, 5, 0]
+a.drop_while { |i| i < 3 }      
+#hash using new hash
+h = { "a" => 100, "b" => 200, "c" => 300 }
+h_new = h.select {|k,v| k > "a"}                     # returns a new hash
+
+
+#iterating methods that take blocks && removes items from data structure until condition in the block evaluates to false
+#array using a new array
+b = [1, 2, 3, 4, 5, 0]
+b.drop_while {|i| i < 3 }   #=> [3, 4, 5, 0]
+#hash 
+# can't find method that meets criteria
 
 
