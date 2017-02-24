adefdcf43475c445028be998e180d6ffbe1b0a1c
J Ted Stamps <tedst@comcast.net>
2017-02-24 15:40:44 -0800

    Clean up code, add summary output, move user input to method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,65 +1,81 @@
-=begin 
-
-psuedocode for release 0 of 5.5 solo challenge
-
-0.  -get "Felicia Torres"
-1.  -Swap first and last name
-    -for each name  
-      -convert into array of char
-2.    -Change all the letters into the next vowel or next consonant 
-        -create an array of vowels (lower case)
-        -create an array of consonants (lower case)
-        -iterate through the array of letters representing the name
-          -check for upper case.  if yes, convert to lower case, and set a flag for later upcasing
-          -if in the vowel array, change the name letter to next indexed vowel. (remember to start at -5)
-          -if in the consonant array, change to next indexed consonant.  (remember to start at -21)
-          -if necessary, upcase the transformed letter.
-      -join the letters back into a single name string
-
+=begin
+pseudocode 
+  get_name
+  Add name to original list
+  add name to fake list
+  with fake name
+    break_whole_name_to_words
+    break_words_to_chars
+    convert_vowels
+    convert_consonants
+    reassemble_words
+    reassemble_whole_name
+    print real and fake name
+print out all name pairs
 =end
 
-#name = "Felicia Torres"
-loop do                                                       #allow users to enter multiple names to encrypt, 
-  puts "Please enter a name to encrypt ('quit' to exit)"
-  name = gets.chomp
-  if name == "quit"  || name == "exit"                    
-    break                               # exit
-  else
-    names_array = name.split(' ')       #split names into array of first and last strings
-    #p names_array                       #debug print
-    names_array = names_array.reverse   #reverse the order of the strings in the array.  may only work for 2 names.
+#DEFINE METHODS
 
-    vowels = ["a", "e", "i", "o", "u"]
-    consonants = "bcdfghjklmnpqrstvwxyz".chars
+def get_name_or_quit
+  user_input = ""
+  puts "Enter a real name, 'quit' to exit."
+  user_input = gets.chomp
+ end
 
-    fake_name = names_array.map do |name|   #for each name in the name_array, change the letters and then write fake names into new array 
-      name_as_letter_array = name.chars     #create array of letters from each name
-      name_as_letter_array.map! do |letter|           #iterate through array one letter at a time
-        if letter != letter.downcase                  #(check if capital, and set boolean
-          is_cap = true
-          letter = letter.downcase
-        else
-          is_cap = false
-        end                  
-        vowels_index = vowels.index(letter)  #check if current letter is in the vowels array, and if so return its index number, else nil
-        if vowels_index != nil                        # if not nil, then overwrite current letter with next vowel in vowels array
-          letter = vowels[vowels_index - 4]           #step backwards in array 4 places to get next array item, so that indexing past "u" does not cause error.  
-        else                                          # is a consonant (no error checking here.  original name must be letters or space, and probably on 2 names)
-          consonants_index = consonants.index(letter)
-          letter = consonants[consonants_index - 20]  #back up index so we don't accidentally increment past z
-        end
-        if is_cap
-          letter = letter.upcase
-        else
-          letter
-        end
-      end
-      name = name_as_letter_array.join
-    end
-  end
-  puts "your new fake name is,  #{fake_name}"
-  puts
-  fake_name
+def add_name(array_of_names, new_name)
+  array_of_names.push(new_name)                   #add new_name to existing array of names.
 end
 
+
+#DEFINE AND INITIALIZE VARIABLES
+vowels = "aeiou".chars                                 #easy way to make array of vowels
+consonants = "bcdfghjklmnpqrstvwxyz".chars             #easy way to make array of consonants
+
+real_names = []       #growing array of the real, whole names (first and last) with space between
+fake_names = []       #growing array of fake names indexed to match the real names 
+whole_name = ""       # string of current working name with space between first and last
+reverse_name_array = []  #temporary array holding place for reversing first and last names
+name_as_letters = []  #array of individual letters for fake name, includes space separator
+
+
+
+#DRIVER CODES
+whole_name = get_name_or_quit                          #initial getting of name and or quit.
+while whole_name != "quit"                             #loop until user enters "quit"
+  add_name(real_names, whole_name)                     #add the real name to existing arrays.  code will modify real name in fake_names
+  reverse_name_array = whole_name.split(" ").reverse   #split first and last name, then reverse order as two strings into reverse_name_array
+  whole_name = reverse_name_array.join(" ")            #first and last name have exchanged places with space between.
+  name_as_letters = whole_name.chars                   #convert name into an array of letters
+  
+  name_as_letters.map! do |letter|                      #for each letter, dangerously do
+    if letter != letter.downcase                        #check for capitalization
+      capital = true
+    else
+      capital = false
+    end
+    letter = letter.downcase                            #downcase all letters
+    if vowels.index(letter) != nil                      # check if current letter is in vowels array
+      letter = vowels[vowels.index(letter) -4]          # change letter to next vowel
+    elsif consonants.index(letter) != nil               # check if current letter is in consonants array
+      letter = consonants[consonants.index(letter) -20] # change letter to next consonant
+    end
+    if capital                                           #if was originally capitalized
+      letter = letter.upcase                             #return it to capital version of letter
+    end
+    letter
+  end
+
+  add_name(fake_names, name_as_letters.join(''))          #join letters back into single string, then add string to fake_names array
+  puts "#{real_names.last} has a fake name of '#{fake_names.last}'"    #give user feedback about the before/after
+  puts
+  whole_name = get_name_or_quit                            #see if user wants to enter a new name or quit.
+end
+
+2.times {puts}                                                         # output spacing
+puts  "Here is the full list of names and their fake name aliases"
+puts
+real_names.each do |name|                                               #for each real name...
+  indx = real_names.index(name)                                           #find it's index number
+  puts "'#{name}' has been encrypted to '#{fake_names[indx]}'"            # then print out the real name and the fake name using the same index
+end
 
