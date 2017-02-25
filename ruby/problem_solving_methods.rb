67e39130fd31247d0c6f9b2b1f0cd0fed1f8c0e3
J Ted Stamps <tedst@comcast.net>
2017-02-24 17:29:01 -0800

    Clean up code from paired session, add additional comments, and add friendly UI so output is more clear

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,115 +1,75 @@
-
-=begin
----------------------------------------------------------
-def find_index(array, int_to_find)
-  found = false
-  array_index = 0
-  while array_index < array.size  && !found
-    if array[array_index] == int_to_find
-      found = true
-    else
-      array_index += 1
-    end
-  array_index
-  end
-end
-
-a = [7,15,2,3,100,11,37]
-i = 7
-
-#DRIVER CODE
-p find_index(a, i)
-
---------------------------------------------------------
-#pseudocode for bubble sort
-
-make second, empty array
-while still elements in first array
-  first element == smallest
-  compare first element to next element
-  if second is smaller, make that new smallest
-  check next element 
-  until end of first array
-
-  push smallest onto second array
-  slice out element from first array
-end
-
---------------------------------------------------------
-
+#---------------------------------------------------------
+# RELEAES 0  SIMPLE SEARCH
 
 #DEFINE METHODS
 def find_index(array, int_to_find)
-  if array.each == int_to_find
-    final_value = array.each
-    p final_value
-  end
-
-#  array.each do |variable|
-    
-  end
-end
-
-
-#DRIVER CODE
-def find_index(array, int_to_find)
-  array_index = 0 
-  found_index = nil 
-  while array_index < array.length 
-    if array[array_index] == int_to_find
-      p array_index
-      found_index = array_index
-      array_index += 1 
-    else
-      array_index += 1 
+  array_index = -1                          # starting at -1 so I can increment at beginning of while loop
+  found_index = nil                         
+  while array_index < array.length          # search every element in the array
+    array_index += 1                        # increment index
+    if array[array_index] == int_to_find    # if array element is equal to the integer we're searching for
+      found_index = array_index                #set the found_index for return
     end 
   end
-  found_index 
+  found_index                                #return the found_index    expect either an integer or a nil
 end
 
 
 #DRIVER CODE
-a = [7,15,2,3,100,11,37]
-i = 3
+a = [7,15,2,3,100,11,37]                    # array with prefilled integers to search
+i = 3                                       # the integer to search for in the array
 
-find_index(a, i)
+found = find_index(a, i)                    # call the method with array "a" and integer "i"
+if found == nil                             # boolean to tell user if 'nil' was returned (instead of blank line)
+  puts "The returned index is 'nil'"
+else 
+  "The returned index is #{found}"           # otherwise, print out the index of the found integer
+end
+
 
 #-------------------------------------------------
 
+#########  Release 1 Code   Fibonacci  ###########
 
-def fib_grow(int)
-  fib = [0,1] 
-  array_index = 2
-    while array_index < int
-    fib.push(fib[-2] + fib[-1])
-    array_index +=1
+#DEFINE METHO"DS
+
+def fib_grow(int)                      
+  fib = [0,1]                        # define base array
+  array_index = 2                    # set index=2 because that will be the index of the next number to push
+    while array_index < int          # our passed integer is also our index limit
+    fib.push(fib[-2] + fib[-1])      #add the last two integers in the current array, then push the result 
+    array_index +=1                  #increment counter
     end
-  fib 
+  fib                                #pass back this modified array 
 end
 
-new_fib = fib_grow(100)
-p new_fib
-puts "last element is #{new_fib.last}"
-=end
+#DRIVER CODE
+new_fib = fib_grow(100)                 #call method feb_grow, with integer, and return the array of fibonacci numbers.
 
-=begin 
-------------------------------------
+if new_fib.last == 218922995834555169026                           # checking expected value for input of 100
+  puts "SUCCESS !!!  The 100th element matches expectations"       #print out success banner
+  puts
+end
+puts "The Fibonacci array for 100 elements is #{new_fib}"          #print out the new array
+
+#------------------------------------
+#BEGIN RELEASE 2  Sort an Array
+
+=begin
+
 #pseudocode for bubble sort
 
+populate firt array with integers
 make second, empty array
 while still elements in first array
   find smallest element in first array
   push that value to end of second array
   find index of that element in first array
   delete element out of first array
-  print out new, sorted array
-
-
-
+print out new, sorted array
 =end
 
-
-a = [47,15,20,23,100,11,37,11]
+a = [47,15,20,23,100,11,37,11]                #original array
 b = []
 
 indx = 0 
@@ -119,5 +79,4 @@ while a.length != 0
   a.delete_at(idx_to_delete)
 end 
 puts "The sorted array is #{b}"
-
 
