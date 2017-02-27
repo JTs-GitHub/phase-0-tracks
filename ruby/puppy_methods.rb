eec6a11149b5efd65a1da87863056ee024ce73a7
J Ted Stamps <tedst@comcast.net>
2017-02-27 10:18:18 -0800

    Add release 0 of assignment 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,40 @@ class Puppy
     toy
   end
 
+  def speak(num_of_barks)
+    puts "woof " * num_of_barks
+  end
+
+  def roll_over
+    puts "rolled over"
+  end
+
+  def dog_years(human_years)
+    p 7 * human_years
+  end
+
+  def new_trick()
+    #new trick stuff  
+  end
+
+  def eats_homework
+    p "puppy has eaten homework"
+  end
+
 end
+
+
+#DRIVER CODE
+
+fido = Puppy.new
+fido.fetch("doll")
+
+fido.speak(3)
+fido.roll_over
+fido.dog_years(5)
+fido.eats_homework
+
+
+
+
 
