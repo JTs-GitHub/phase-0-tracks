4ce1ee554cda590f9153b65ccfea5677d074fa06
J Ted Stamps <tedst@comcast.net>
2017-03-03 11:56:11 -0800

    Add and test getter and setter methods for release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,14 +26,20 @@ class Santa
     @ethnicity
   end
 
+  def gender
+    @gender
+  end
+
   # setter methods 
-  def celebrate_birthday=(age)
+  def celebrate_birthday
     puts "Happy Birthday!"
     @age = age + 1
   end
 
   def get_mad_at=(raindeer_name)
-    #change @deer_ranking array order
+    @deer_ranking.delete(raindeer_name)           #remove it from whereever it is
+    @deer_ranking << raindeer_name
+    puts @deer_ranking
   end
 
   def new_gender=(gender)
@@ -44,21 +50,25 @@ end
 
 santas = []
 #DRIVER CODE
-year_2016_santa = Santa.new("m", "x")
+#year_2016_santa = Santa.new("m", "x")
 #year_2016_santa.speak
 #year_2016_santa.eat_milk_and_cookies("sugar")
 
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])    #random gender, random ethnicity
-end   
-p santas[0]
-p santas[1]
-p santas[2]
-puts "age is #{year_2016_santa.age}"
-puts "ethnicity is #{year_2016_santa.ethnicity}"
-p year_2016_santa.new_gender=("tec's choice")
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])    #random gender, random ethnicity
+#end   
+bill = Santa.new("m", "x")
+puts "age is #{bill.age}"
+bill.celebrate_birthday
+puts "age is now #{bill.age}"
+puts "gender is #{bill.gender}"
+puts "Bill is changing his gender to #{bill.new_gender=('self choice')}"
+puts "gender is now #{bill.gender}"
+bill.get_mad_at=("Dasher")
+
+
 
 
 
