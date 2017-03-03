107d68db342826c1a707aabbec2aace728e34ebc
J Ted Stamps <tedst@comcast.net>
2017-03-03 14:04:16 -0800

    Add attr_reader and attr_accessor candy for release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 
 class Santa
+  attr_reader :age, :ethnicity              #getter methods
+  attr_accessor  :gender 
 
   def initialize (gender, ethnicity)
     p "Initializing santa instance ..."
@@ -17,20 +19,6 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
-  # getter methods
-  def age
-    @age 
-  end
-
-  def ethnicity
-    @ethnicity
-  end
-
-  def gender
-    @gender
-  end
-
-  # setter methods 
   def celebrate_birthday=(years)
     puts "Happy Birthday number #{years}!"
     @age = years 
@@ -39,21 +27,15 @@ class Santa
   def get_mad_at=(raindeer_name)
     puts "old deer ranking was #{@deer_ranking}"
     @deer_ranking.delete(raindeer_name)           #remove it from wherever it is
-    @deer_ranking << raindeer_name
+    @deer_ranking << raindeer_name                #then add it to the end of the shortened array
     puts "New deer ranking is #{@deer_ranking}"
   end
 
-  def new_gender=(gender)
-    @gender = gender
-  end
 
 end
 
 santas = []
 #DRIVER CODE
-#year_2016_santa = Santa.new("m", "x")
-#year_2016_santa.speak
-#year_2016_santa.eat_milk_and_cookies("sugar")
 
 #example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 #example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -61,13 +43,16 @@ santas = []
 #  santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])    #random gender, random ethnicity
 #end   
 bill = Santa.new("m", "x")
+
 puts "age is #{bill.age}"
-bill.celebrate_birthday = 3
+bill.celebrate_birthday = 3                                              #setter method using tradition equation
 puts "age is now #{bill.age}"
+
 puts "gender is #{bill.gender}"
-puts "Bill is changing his gender to #{bill.new_gender='self choice'}"
+puts "Bill is changing his gender to #{bill.gender='self choice'}"       #setter method called within interpolation
 puts "gender is now #{bill.gender}"
-bill.get_mad_at = "Dasher"
+
+bill.get_mad_at = "Dasher"                                               #setter method using traditional equation
 
 
 
