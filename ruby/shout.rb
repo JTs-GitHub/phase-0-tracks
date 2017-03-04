f01068353b14e63632ce7d67a7a703d9f22ec0be
J Ted Stamps <tedst@comcast.net>
2017-03-04 14:16:54 -0800

    Convert standalone module to mixin, Module 6.4  Release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,5 @@
 #Assignment 6.4
-
+=begin 
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -14,3 +14,24 @@ Shout.singleton_methods
 puts "------------"
 puts Shout.yell_angrily("hi there.   how are you")
 puts Shout.yelling_happily("I'm good, thanks for asking")
+=end
+
+module Shout
+  def outside_voice(words)
+    "!!!    " + words.upcase + "!!!"
+  end
+end
+
+class Cop
+  include Shout 
+end
+
+class Carnival_Barker
+  include Shout
+end
+
+officer = Cop.new
+puts officer.outside_voice("halt or I'll shoot")
+puts "------------"
+carnie = Carnival_Barker.new
+puts carnie.outside_voice("Come see the most amazing site in the history of mankind...")
