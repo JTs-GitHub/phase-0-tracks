9298c8882383082b49bf92fcfb37ebc86d1e3605
J Ted Stamps <tedst@comcast.net>
2017-03-10 13:33:44 -0800

    Add release 0 of Module 7.3, Finding long phrase in an array of phrases. With commented pseudocode.

---
+++ js/algos.js
@@ -0,0 +1,33 @@
+/*
+
+PsuedoCode of Release 0:  Find the Longest Phrase in an array of phrases/words
+------------------------
+Start with longest phrase = 0
+While not at end of phrase list
+  Get next word/phrase
+  Find the length of current word/phrase
+  If length of current word/phrase is longer than old 'longest' phrase
+    update the longest phrase to be the current phrase/word
+  if at last phrase on list, exit while loop
+  else go to top of while loop 
+Return longest word/phrase
+
+*/
+
+function findLongest(phrase_list) {
+  var longestIndex = 0;
+  var longestLength = 0;
+
+  for (var i = 0; i < phrase_list.length; i++) {
+    if (phrase_list[i].length > longestLength) {
+      longestLength = phrase_list[i].length;
+      longestIndex = i; 
+    }
+  }
+  return longestIndex ;
+}
+
+var phrases = ["long phrase", "longest phrase", "longer phrase", "tiny", "When in the course of human events..."]
+
+indexOfLongest = findLongest(phrases)
+console.log("the longest phrase is '" + phrases[indexOfLongest] + "'")
