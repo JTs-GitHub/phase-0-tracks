43dcb0ff62579426f91db87f48e4870d8d64fc0e
J Ted Stamps <tedst@comcast.net>
2017-03-10 10:59:20 -0800

    Add create object code, and build object with names of horses and colors of horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,16 @@
-var colors = ["blue", "green", "red", "yellow"]
-var names = ["Mr. Ed", "Seattle Slew", "Secretariat", "Old Ironsides"]
+var colors = ["blue", "green", "red", "yellow"];
+var names = ["Mr. Ed", "Seattle Slew", "Secretariat", "Old Ironsides"];
 
-colors.push("Chrome")
-names.push("Chrome")
+colors.push("Chrome");
+names.push("Chrome");
 
-console.log(colors)
-console.log(names)
+//console.log(colors);
+//console.log(names);
+
+var horse = {};
+
+for (var i = 0; i < colors.length; i++) {
+  horse[names[i]] = colors[i];
+}
+
+console.log(horse);
