3a1e5795fcf28949f3f52935677c92127458ba99
J Ted Stamps <tedst@comcast.net>
2017-03-10 19:35:24 -0800

    Add function to compare 2 objects for matching key value pairs.  Release 1 of module 7.3

--- js/algos.js
+++ js/algos.js
@@ -12,8 +12,6 @@ While not at end of phrase list
   else go to top of while loop 
 Return longest word/phrase
 
-*/
-
 function findLongest(phrase_list) {
   var longestIndex = 0;
   var longestLength = 0;
@@ -31,3 +29,48 @@ var phrases = ["long phrase", "longest phrase", "longer phrase", "When in the co
 
 //indexOfLongest = findLongest(phrases)
 console.log("the longest phrase is '" + findLongest(phrases) + "'")
+*/
+//-----------------------------------------------------------------------------------------
+
+/*
+
+pseudocode of Release 1: Find a Key-Value Match
+------------------------
+
+Create two objects.
+create a function that accepts two objects as parameters.
+Check each key/value pair in both objects to determine of they share AT LEAST ONE common key/value.
+  Iterate through all the keys in first object.
+    For each key in the first object, 
+      determine if the second object contains the same key.
+      if yes, 
+        then check the two values of the matching keys.
+        If they are equal, your function is done and needs to return TRUE
+        Otherwise, go get the next key in the loop
+  If no matches found, return false
+*/
+//obj.values 
+
+function hasMatchingKeyValues(object1, object2) {
+  var array1keys = Object.keys(object1);
+  var array1values = Object.values(object1);
+  var match = false;
+  var key = "";
+  // step through each element in the first array and see if it is in the second array
+  for (var i = 0; i < array1keys.length; i++) {
+    key = array1keys[i];
+    if ((object2.hasOwnProperty(key)) && (object1[key] == object2[key])) {
+      match = true;
+    }
+  }
+  return match 
+}
+
+var object1 = {name: "JT", age: 21, favFood: "ice creams", car: "van", shoe: "brogue" };
+var object2 = {age: 22, married: true, kids: true, favFood: "ice cream"};
+
+if (hasMatchingKeyValues(object1, object2)) {
+  console.log("yes, we have a match"); 
+} else {
+    console.log("there was no match");
+}
