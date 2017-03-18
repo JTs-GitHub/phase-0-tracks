38a5c340bcc7daa6d076147f7a5886003907bcb0
J Ted Stamps <tedst@comcast.net>
2017-03-17 23:50:41 -0700

    Start adding basic UI code for user login and simple menu skeleton

--- databases/serendipity/serendipity_user.rb
+++ databases/serendipity/serendipity_user.rb
@@ -33,41 +33,76 @@ def rand_mem_numb              # 1-100 integer equates to member number
   rand(100) + 1 
 end
 
-# DRIVER CODE *************************************************************************************
-
-#  test code that will randomly jumble existing member location. 200 random reps is ~ 90% of records updated.
-200.times do
-    update_member_location(db, rand_mem_numb, rand_long, rand_lat)  
+# randomly shuffle members to new locations, simulating a gap in time when user gets the latest db results.
+def update_all_members
+  200.times do
+    update_member_location(db, rand_mem_numb, rand_long, rand_lat)
+  end
 end
 
-#get array of everybody and their location, including current user.
-locations = db.execute("select name, longitude, latitude from serendipity_members ;")  # this gets array of everybody 
 
-puts ("Please enter your membership number...")
-current_member_id = 1
+
+# DRIVER CODE *************************************************************************************
+
+#update_all_members
+
+puts ("Welcome to Serendipity 2017:   the social networking program that gives 'chance' a slightly higher profile.")
+puts ("The goal of Serendipity Inc. is to let each of its members know if they are close to other members of their social network.")
+puts ("Serendipity will tell you if a friend or co-worker is at the same sporting event, or on the same BART, or at a coffee shop just down the street...")
+puts ("In the near future, you can be part of multiple groups, each one notifying you and them of your proximity.")
+puts 
+puts ("Please enter your merbership number:")
+mem_numb = gets.chomp.to_i 
+puts mem_numb 
+name = db.execute("select name from serendipity_members where id=#{mem_numb} ;")  # this gets array of array of name.  name = [["Ted Stamps"]] 
+puts ("Welcome back #{name[0][0]}")
+puts
+puts
+puts ("What would you like to do?")
+
+while true 
+  puts
+  puts ("enter 'c' to check for current status, 'u' to update your current location, 'a' to update all status, or 'q' to quit:")
+  user_choice = gets.chomp
+  puts
+  if user_choice == 'c'
+    puts user_choice
+  elsif user_choice == 'u' 
+    puts "u"
+  elsif user_choice == 'a' 
+    puts "a"
+  elsif user_choice == 'q'
+    puts ("Thanks for using Serendipity 2017")
+    break
+  end
+end 
+
+#get array of everybody and their location, including current user.
+#locations = db.execute("select name, longitude, latitude from serendipity_members ;")  # this gets array of everybody 
+
 
 # get array of everybody except the current user => locations[0][0]== name, locations[0][1]== longitude, locations[0][2] == latitude 
 #locations = db.execute("select name, longitude, latitude from serendipity_members where id != #{current_member_id};")  
 
-puts ("locations[0][0] is #{locations[0][0]}")
-puts ("locations[0][1] is #{locations[0][1]}")
-puts ("locations[0][2] is #{locations[0][2]}")
-puts ("locations[1][0] is #{locations[1][0]}")
-puts ("locations[1][1] is #{locations[1][1]}")
-puts ("locations[1][2] is #{locations[1][2]}")
+# puts ("locations[0][0] is #{locations[0][0]}")
+# puts ("locations[0][1] is #{locations[0][1]}")
+# puts ("locations[0][2] is #{locations[0][2]}")
+# puts ("locations[1][0] is #{locations[1][0]}")
+# puts ("locations[1][1] is #{locations[1][1]}")
+# puts ("locations[1][2] is #{locations[1][2]}")
 
-p locations[1]
+# p locations[1]
  
 
 #distance = Haversine.distance(locations[0][1], locations[0][2], locations[1][1], locations[1][2])
 #puts distance.to_miles
 
-locations.each do |x| 
-  distance = Haversine.distance(x[1], x[2], locations[current_member_id][1], locations[current_member_id][2])
-  if (distance.to_feet < 3000) & (x[0] != 'Felipa Gulgowski') 
-    puts ("GREAT NEWS , #{x[0]} is less than 3000 feet away!")
-  end
-end
+# locations.each do |x| 
+#   distance = Haversine.distance(x[1], x[2], locations[current_member_id][1], locations[current_member_id][2])
+#   if (distance.to_feet < 3000) & (x[0] != 'Felipa Gulgowski') 
+#     puts ("GREAT NEWS , #{x[0]} is less than 3000 feet away!")
+#   end
+# end
 
 #  puts ("distance to #{member[0]} is #{distance.to_miles} miles.")
 
